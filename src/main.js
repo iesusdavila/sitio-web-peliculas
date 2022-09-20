9ffcafcb1ef306c427b9859ee1988bcc21cbf7cc
iesusdavila <irdavila@espol.edu.ec>
2022-09-20 13:03:25 -0500

    Scroll infinito en Trends Movies

--- src/main.js
+++ src/main.js
@@ -11,7 +11,6 @@ const api = axios.create({
 
 const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach((entry) => {
-    console.log(observer);
     if (entry.isIntersecting) {
       const urlImg = entry.target.getAttribute("data-image");
       entry.target.setAttribute("src", urlImg);
@@ -252,8 +251,10 @@ async function getMoviesBySearch(query) {
 
 async function getTrendingMovies() {
   const { data } = await api("trending/movie/day");
-
   const movies = data.results;
+
+  pageMax = data.total_pages;
+  console.log(pageMax);
 
   const trendingPreview_movieList = document.querySelector(
     ".trendingPreview-movieList"
@@ -281,6 +282,44 @@ async function getTrendingMovies() {
     movie_container.appendChild(imageMovie);
     genericSection.appendChild(movie_container);
   });
+}
+
+async function getPaginatedTrendingMovies() {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  const scrollIsBottom = scrollTop + clientHeight >= scrollHeight - 15;
+
+  if (scrollIsBottom && page < pageMax) {
+    page++;
+    const { data } = await api("trending/movie/day", {
+      params: {
+        page,
+      },
+    });
+    const movies = data.results;
+
+    movies.forEach((movie) => {
+      const movie_container = document.createElement("div");
+      movie_container.classList.add("movie-container");
+      movie_container.addEventListener("click", () => {
+        location.hash = "#movie=" + movie.id;
+      });
+
+      const imageMovie = document.createElement("img");
+      imageMovie.classList.add("movie-img");
+      imageMovie.alt = movie.title;
+      imageMovie.setAttribute(
+        "data-image",
+        "https://image.tmdb.org/t/p/w300/" + movie.poster_path
+      );
+
+      observer.observe(imageMovie);
+
+      movie_container.appendChild(imageMovie);
+      genericSection.appendChild(movie_container);
+    });
+    //createMovies(movies, genericSection, { lazyLoad: true, clean: false });
+  }
 }
 
 async function getMovieById(id) {
--- src/navigation.js
+++ src/navigation.js
@@ -1,3 +1,7 @@
+let maxPage;
+let page = 1;
+let infiniteScroll;
+
 searchFormBtn.addEventListener("click", () => {
   location.hash = "#search=" + searchFormInput.value;
 });
@@ -13,10 +17,14 @@ arrowBtn.addEventListener("click", () => {
 
 window.addEventListener("DOMContentLoaded", navigator, false);
 window.addEventListener("hashchange", navigator, false);
+window.addEventListener("scroll", infiniteScroll, false);
 
 function navigator() {
   console.log({ location });
-
+  if (infiniteScroll) {
+    window.removeEventListener("scroll", infiniteScroll, { passive: false });
+    infiniteScroll = undefined;
+  }
   if (location.hash.startsWith("#trends")) {
     trendsPage();
   } else if (location.hash.startsWith("#search=")) {
@@ -31,6 +39,10 @@ function navigator() {
 
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+
+  if (infiniteScroll) {
+    window.addEventListener("scroll", infiniteScroll, { passive: false });
+  }
 }
 
 function homePage() {
@@ -125,4 +137,6 @@ function trendsPage() {
   text_footer.classList.remove("inactive");
 
   getTrendingMovies();
+
+  infiniteScroll = getPaginatedTrendingMovies;
 }
